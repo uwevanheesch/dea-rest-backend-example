653db3b84ea9193a9d391f9b600bb617234422d2
Uwe van Heesch <uwe@vanheesch.net>
2020-03-19 14:07:25 +0100

    - extract interface for authentication service

--- src/main/java/nl/han/dea/service/AuthenticationService.java
+++ src/main/java/nl/han/dea/service/AuthenticationService.java
@@ -1,33 +1,10 @@
 package nl.han.dea.service;
 
-import nl.han.dea.persistence.AuthenticationDAO;
 import nl.han.dea.resource.LoginDTO;
 import nl.han.dea.resource.TokenDTO;
 
-import javax.inject.Inject;
-import java.util.UUID;
+public interface AuthenticationService {
+    TokenDTO authenticate(LoginDTO loginDTO);
 
-public class AuthenticationService {
-
-    private AuthenticationDAO authenticationDAO;
-
-    public TokenDTO authenticate(LoginDTO loginDTO) {
-        if (authenticationDAO.accountExists(loginDTO)) {
-
-            String token = UUID.randomUUID().toString();
-            authenticationDAO.addTokenForUser(token, loginDTO.getUser());
-            return new TokenDTO(loginDTO.getUser(), token);
-        } else {
-            throw new AuthenticationFailedException();
-        }
-    }
-
-    public String getUsernameForToken(String token) {
-        return authenticationDAO.getUsernameForToken(token);
-    }
-
-    @Inject
-    public void setAuthenticationDAO(AuthenticationDAO authenticationDAO) {
-        this.authenticationDAO = authenticationDAO;
-    }
+    String getUsernameForToken(String token);
 }
---
+++ src/main/java/nl/han/dea/service/AuthenticationServiceImpl.java
@@ -0,0 +1,35 @@
+package nl.han.dea.service;
+
+import nl.han.dea.persistence.AuthenticationDAO;
+import nl.han.dea.resource.LoginDTO;
+import nl.han.dea.resource.TokenDTO;
+
+import javax.inject.Inject;
+import java.util.UUID;
+
+public class AuthenticationServiceImpl implements AuthenticationService {
+
+    private AuthenticationDAO authenticationDAO;
+
+    @Override
+    public TokenDTO authenticate(LoginDTO loginDTO) {
+        if (authenticationDAO.accountExists(loginDTO)) {
+
+            String token = UUID.randomUUID().toString();
+            authenticationDAO.addTokenForUser(token, loginDTO.getUser());
+            return new TokenDTO(loginDTO.getUser(), token);
+        } else {
+            throw new AuthenticationFailedException();
+        }
+    }
+
+    @Override
+    public String getUsernameForToken(String token) {
+        return authenticationDAO.getUsernameForToken(token);
+    }
+
+    @Inject
+    public void setAuthenticationDAO(AuthenticationDAO authenticationDAO) {
+        this.authenticationDAO = authenticationDAO;
+    }
+}
